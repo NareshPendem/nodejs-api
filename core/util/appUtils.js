98bffeff43ff7c23c7df7283d7270a02e31a915e
npendem <naresh.333@gmail.com>
2019-07-07 01:33:02 -0600

    Consolidated Loggin from multiple APIs for Deribit.

--- core/util/appUtils.js
+++ core/util/appUtils.js
@@ -18,16 +18,33 @@ exports.getApiUrlToConsume = () =>{
   return "";
 }
 
+exports.getApiUrlToConsume_chained = () => {
+  if (config.get(constants.CONFIG_ENTRY_CURRENT_EXCHANGE) === "binance") {
+    return "";
+  } else if (config.get(constants.CONFIG_ENTRY_CURRENT_EXCHANGE) === "deribit") {
+    return config.get(constants.CONFIG_ENTRY_DERIBIT_BTC_PERPETUAL_URL);
+  }
+  console.log("Unable find the active exchange..");
+  return "";
+}
+
 exports.getFormattedTimeStamp = () => {
   var data = timestamp.utc('YYYY') + "-" + timestamp.utc('MM') + "-" + timestamp.utc('DD') + "-" +
     timestamp.utc('HH') + "-" + timestamp.utc('mm') + "-" + timestamp.utc('ss');
     return data;
 }
 
-exports.buildJSONObjectForLogging = (respObj) => {
-
+exports.buildJSONObjectForLogging = (respObj,jsonObjectForLogging) => {
 
   var formattedTS = this.getFormattedTimeStamp();
+
+  if (jsonObjectForLogging !== null
+    && jsonObjectForLogging !== ""
+     && jsonObjectForLogging.hasOwnProperty('timestamp')){
+    var key = respObj.symbol;
+    jsonObjectForLogging[key] = respObj.price;
+    return jsonObjectForLogging;
+  }
 
   const json = {
     timestamp: formattedTS,
